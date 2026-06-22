a45f7bab93ed066ed3707e33dc073f35a8fd69b6
omansh2005 <omanshsrivastava2005@gmail.com>
2026-06-22 11:24:01 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (96.97%) - LeetSync

---
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int p1 = m - 1;       // End of actual elements in nums1
+        int p2 = n - 1;       // End of nums2
+        int p = m + n - 1;    // End of total capacity in nums1
+        
+        // Merge from the back to avoid overwriting elements
+        while (p1 >= 0 && p2 >= 0) {
+            if (nums1[p1] > nums2[p2]) {
+                nums1[p] = nums1[p1];
+                p1--;
+            } else {
+                nums1[p] = nums2[p2];
+                p2--;
+            }
+            p--;
+        }
+        
+        // If there are remaining elements in nums2, copy them
+        while (p2 >= 0) {
+            nums1[p] = nums2[p2];
+            p2--;
+            p--;
+        }
+    }
+};
